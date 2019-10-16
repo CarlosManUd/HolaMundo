fabc8dc495032d68ebf983c5b768e8be729d4bdc
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-10-16 13:42:19 +0200

    commitsssssss

---
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,12 @@
+package hola_mundo;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+
+     public class Hola_Mundo {
+	public static void main(String[] args){
+		System.out.println("¡Hola Mundo!");
+	}
+}
